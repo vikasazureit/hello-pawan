fde9eb7f4e1176f848d00aa147c4d69ec7f26017
vikasazureit <vikas.azureit@gmail.com>
2026-08-24 15:56:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
     const now = new Date();
